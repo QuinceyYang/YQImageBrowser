22b4faa35c94c9ed1abdad8c8b084daec6390b6a
QuinceyYang <yqing555@qq.com>
2018-11-30 16:36:55 +0800

    yqing:Initial

---
+++ YQImageBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQImageBrowser
+//
+//  Created by 杨清 on 2018/11/30.
+//  Copyright © 2018 QuinceyYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YQImageBrowser/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  YQImageBrowser
+//
+//  Created by 杨清 on 2018/11/30.
+//  Copyright © 2018 QuinceyYang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
