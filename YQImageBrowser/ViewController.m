56b075e12724e0de01551ad419af2e30f4b430bb
QuinceyYang <yqing555@qq.com>
2018-12-04 16:27:28 +0800

    yqing:浏览网络高清图片

--- YQImageBrowser/ViewController.m
+++ YQImageBrowser/ViewController.m
@@ -20,18 +20,27 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
-    button.backgroundColor = [UIColor greenColor];
+    button.backgroundColor = [UIColor blueColor];
+    button.titleLabel.font = [UIFont boldSystemFontOfSize:19];
     [button setTitle:@"show" forState:UIControlStateNormal];
     button.center = self.view.center;
-    button.bounds = CGRectMake(0, 0, 100, 100);
+    button.bounds = CGRectMake(0, 0, 110, 110);
     [button addTarget:self action:@selector(show) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button];
-
+    button.layer.cornerRadius = button.frame.size.height/2;
+    button.layer.masksToBounds = YES;
 }
 
 - (void)show {
-#if 0
-
+#if 1
+    //由于使用的网络高清图片，所以第一次加载时比较慢
+    NSArray *array = @[@"https://raw.githubusercontent.com/QuinceyYang/YQImageBrowser/master/YQImageBrowser/Resource/01.jpg",
+                       @"https://raw.githubusercontent.com/QuinceyYang/YQImageBrowser/master/YQImageBrowser/Resource/02.jpg",
+                       @"https://raw.githubusercontent.com/QuinceyYang/YQImageBrowser/master/YQImageBrowser/Resource/03.jpg",
+                       @"https://raw.githubusercontent.com/QuinceyYang/YQImageBrowser/master/YQImageBrowser/Resource/04.jpg",
+                       @"https://raw.githubusercontent.com/QuinceyYang/YQImageBrowser/master/YQImageBrowser/Resource/05.jpg",
+                       @"https://raw.githubusercontent.com/QuinceyYang/YQImageBrowser/master/YQImageBrowser/Resource/06.jpg"
+                       ];
 #else
     NSMutableArray *array = [NSMutableArray array];
     for (NSUInteger i=0; i<6; i++) {
